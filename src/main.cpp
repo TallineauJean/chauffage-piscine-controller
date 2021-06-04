6481c5254cc2986490e2f4fed9c4c30ce6888b05
Jean <jean.tallineau@rca.fr>
2021-06-04 14:48:26 +0200

    :wrench: configure le websocket

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,16 @@
 #include <temperature.h>
 #include <http.h>
 #include <analogWrite.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
 
 #define LED_PIN 2
 
 #define RGB_G 12
 #define RGB_B 13
 #define RGB_R 14
+
+#define RELAIS 27
 
 // preferences -> https://www.tutorialspoint.com/esp32_for_iot/esp32_for_iot_preferences.htm
 // ESPasync -> https://github.com/me-no-dev/ESPAsyncWebServer
@@ -22,7 +26,42 @@ Temperature temperature;
 WiFiClient wifi;
 HttpClient httpClient = HttpClient(wifi, "192.168.1.16", 8888);
 
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
 int status = WL_IDLE_STATUS;
+
+void notifyClients() {
+  ws.textAll(String("bonjour"));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
 
 void setup(){
   Serial.begin(115200);
@@ -44,7 +83,17 @@ void setup(){
   pinMode(RGB_R, OUTPUT);
   pinMode(RGB_G, OUTPUT);
   pinMode(RGB_B, OUTPUT);
+  pinMode(RELAIS, OUTPUT);
+  digitalWrite(RELAIS, LOW);
 
+  analogWrite(RGB_R, 128, 128);
+
+  if(!SPIFFS.begin(true)){
+      log_e("An Error has occurred while mounting SPIFFS");
+    }
+
+  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html").setCacheControl("max-age=6000");
+  server.begin();
 }
 
 void loop(){
@@ -62,50 +111,32 @@ void loop(){
 
     int gain = temperatureEauSortie - temperatureEauEntree;
     if(gain > 0){
-      analogWrite(RGB_R, 128, 128);
-      analogWrite(RGB_G, 50, 128);
+     // analogWrite(RGB_R, 128, 128);
+      digitalWrite(RELAIS, HIGH);
+      analogWrite(RGB_G, 0, 128);
       analogWrite(RGB_B, 0, 128);
     } else if (gain < 0){
-      analogWrite(RGB_R, 50, 128);
+     // analogWrite(RGB_R, 50, 128);
+      digitalWrite(RELAIS, LOW);
+      analogWrite(RGB_G, 0, 128);
+      analogWrite(RGB_B, 128, 128);
+    } else {
+     // analogWrite(RGB_R, 0, 128);
+      digitalWrite(RELAIS, LOW);
       analogWrite(RGB_G, 128, 128);
       analogWrite(RGB_B, 0, 128);
-    } else {
-      analogWrite(RGB_R, 0, 128);
-      analogWrite(RGB_G, 50, 128);
-      analogWrite(RGB_B, 128, 128);
     }
 
-
     Serial.println(log);
-  
+
+
+    // WS
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+
+
+    char data[18];
+    snprintf(data, 18, "%.2f;%.2f;%.2f", temperatureEauEntree, temperatureEauSortie, temperatureAir);
+    ws.textAll(String(data));
+
 }
-
-
-
-
-
-
-
-
-  // delay(3000);
-
-  // char data[10];
-      // snprintf(data, 10, "temp=%f", temp);
-
-  //String postData = "temperature=21.9";
-  // client.beginRequest();
-  // client.post("/temperature");
-  // client.sendHeader("Content-Type", "application/x-www-form-urlencoded");
-  //client.sendHeader("Content-Length", postData.length());
-  //client.sendHeader("X-Custom-Header", "custom-header-value");
-  // client.beginBody();
-  // client.print(postData);
-  //client.endRequest();
-
-  //int statusCode = client.responseStatusCode();
-  // String response = client.responseBody();
-
-  // Serial.print("Status code: ");
-  // Serial.println(statusCode);
-  // Serial.print("Response: ");
-  // Serial.println(response);
