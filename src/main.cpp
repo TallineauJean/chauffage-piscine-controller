fa41d0622ac662982fb0cb917965e4439f72efa5
Jean <jean.tallineau@rca.fr>
2021-06-04 18:43:28 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,6 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
-#define LED_PIN 2
-
-#define RGB_G 12
-#define RGB_B 13
-#define RGB_R 14
-
-#define RELAIS 27
-
 // preferences -> https://www.tutorialspoint.com/esp32_for_iot/esp32_for_iot_preferences.htm
 // ESPasync -> https://github.com/me-no-dev/ESPAsyncWebServer
 // ESPDIFF -> https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/storage/spiffs.html
@@ -80,14 +72,6 @@ void setup(){
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 
-  pinMode(RGB_R, OUTPUT);
-  pinMode(RGB_G, OUTPUT);
-  pinMode(RGB_B, OUTPUT);
-  pinMode(RELAIS, OUTPUT);
-  digitalWrite(RELAIS, LOW);
-
-  analogWrite(RGB_R, 128, 128);
-
   if(!SPIFFS.begin(true)){
       log_e("An Error has occurred while mounting SPIFFS");
     }
@@ -97,46 +81,18 @@ void setup(){
 }
 
 void loop(){
-    delay(1000);
+    delay(10000);
     float listeTemperatures[3];
 
     temperature.releverTemperatureSurSonde(listeTemperatures);
     float temperatureEauEntree = listeTemperatures[0];
     float temperatureEauSortie = listeTemperatures[1];
     float temperatureAir = listeTemperatures[2];
-
-    char log[74];
-    snprintf(log, 74, "Temp eau entrée -> %.2f -- Temp eau sortie -> %.2f -- Temp air -> %.2f", temperatureEauEntree, temperatureEauSortie, temperatureAir);
     
-
-    int gain = temperatureEauSortie - temperatureEauEntree;
-    if(gain > 0){
-     // analogWrite(RGB_R, 128, 128);
-      digitalWrite(RELAIS, HIGH);
-      analogWrite(RGB_G, 0, 128);
-      analogWrite(RGB_B, 0, 128);
-    } else if (gain < 0){
-     // analogWrite(RGB_R, 50, 128);
-      digitalWrite(RELAIS, LOW);
-      analogWrite(RGB_G, 0, 128);
-      analogWrite(RGB_B, 128, 128);
-    } else {
-     // analogWrite(RGB_R, 0, 128);
-      digitalWrite(RELAIS, LOW);
-      analogWrite(RGB_G, 128, 128);
-      analogWrite(RGB_B, 0, 128);
-    }
-
-    Serial.println(log);
-
-
     // WS
     ws.onEvent(onEvent);
     server.addHandler(&ws);
-
-
     char data[18];
     snprintf(data, 18, "%.2f;%.2f;%.2f", temperatureEauEntree, temperatureEauSortie, temperatureAir);
     ws.textAll(String(data));
-
 }
